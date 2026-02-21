5bfdc0849f187374b038898abf1debd6b0965b36
jtroussard <jacques.troussard@gmail.com>
2026-02-21 13:00:16 -0500

    init commit

---
+++ src/main/java/com/devlife4me/projectvital/ProjectvitalApplication.java
@@ -0,0 +1,13 @@
+package com.devlife4me.projectvital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectvitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectvitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlife4me/projectvital/ProjectvitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlife4me.projectvital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectvitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
